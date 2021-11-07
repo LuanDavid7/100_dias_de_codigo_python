04a9643a18583ca03a851664a96d9213d9ede0c6
Luan David <luandavidsp@gmail.com>
2021-11-07 19:22:20 -0300

    commit inicial

--- day02/main.py
+++ day02/main.py
@@ -5,41 +5,41 @@ base_bill = ""
 percentage = ""
 people = ""
 
-print("Welcome to the tip calculator.")
+print("Seja bem vindo a Calculadora de Gorjeta.")
 
 # get the base bill without tip
 while True:
-    print("What was the total bill?")
-    base_bill = input("> $")
+    print("Quanto ficou a conta?")
+    base_bill = input("> R$ ")
     # check if it can be converted to a float
     try:
         float(base_bill)
         # check if it's not a negative number
         # zero is fine, as they might be eating for free... not much sense asking for the other inputs then though
         if float(base_bill) < 0:
-            print("The bill can't be negative. Please enter a valid amount.")
+            print("A conta não pode ser negativa, por favor insira um valor válido.")
         # otherwise it should be fine
         else:
             break
     # if not, ask again
     except ValueError:
-        print("Please enter a valid amount.")
+        print("Insira um valor válido.")
 
 # get the percentage
 while True:
-    print("What percentage tip would you like to give? 10, 12, or 15?")
+    print("Quantos % de gorjeta gostaria de deixar? 3, 5 ou 7?")
     percentage = input("> ")
     # list of acceptable choices
-    choices_list = ["10", "12", "15"]
+    choices_list = ["3", "5", "7"]
     # make sure the input is one of them
     if percentage not in choices_list:
-        print("Please enter 10, 12 or 15.")
+        print("Digite um valor que seja 3, 5 ou 7.")
     else:
         break
 
 # get the number of people
 while True:
-    print("How many people to split the bill?")
+    print("Quantas pessoas vão dividir a conta?")
     people = input("> ")
     # make sure it's a non-zero natural number
     if not people.isdigit() or people == "0":
@@ -55,4 +55,4 @@ total_bill = float(base_bill) * (1 + int(percentage) / 100)
 split_bill = round(total_bill / int(people), 2)
 
 # using a uniform message, even if there was just a single person
-print(f"Each person should pay: ${split_bill}")
+print(f"Cada pessoa deve pagar: R${split_bill}")
